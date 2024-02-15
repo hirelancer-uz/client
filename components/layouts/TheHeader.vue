4ee93ab5194270ab7e3a516c0c6be65cfe50f533
ChKhasan <chulliyevhasan1405@mail.ru>
2024-02-15 14:38:48 +0500

    login text

--- components/layouts/TheHeader.vue
+++ components/layouts/TheHeader.vue
@@ -157,7 +157,7 @@
             {{
               $store.state.auth && Boolean($store.state.userInfo["name"])
                 ? $store.state.userInfo["name"]
-                : "Chiqish"
+                : "Kirish"
             }}
           </button>
           <button
